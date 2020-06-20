2bd470a536afa61fb2ebad70ad865b631a612029
danny <danny@MacBook-Pro.local>
2020-06-20 12:06:35 -0400

    hopefully this works

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ const app = express()
       app.use(bodyParser.json())
 
 
-      const path = require('path');
-      app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-      });
-    }
+      // this makes sure express behaves correctly
+      if (process.env.NODE_ENV === 'production') {
+        // Express will serve up production assets
+        // like our main.js file, or main.css file!
+        app.use(express.static('client/build')); //118
+
+        // Express will serve up the index.html file
+        // if it doesn't recognize the route
+         //express will serve up the index.html if it doesnt recognize the route
+        const path = require('path');
+        app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+      }
+
 
 const PORT = process.env.PORT || 5000
 
