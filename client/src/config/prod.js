4ffa1c49c3b66b7d15c6a8957f52264c1da72654
danny <danny@MacBook-Pro.local>
2020-06-21 17:18:00 -0400

    backend update

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,5 +1,6 @@
 //DONT COMMIT, THIS IS PRODUCTION
 
 module.exports = {
-  googleApiKey : process.env.GOOGLE_API_KEY
+  googleApiKey : process.env.GOOGLE_API_KEY,
+  mapBoxApi : process.env.MAP_BOX_API
 }
