3a8578b6e6eac3bef936800a81182dd50f953ce2
danny <danny@Dannys-MacBook-Pro.local>
2020-09-03 20:31:31 -0400

    cleaned up code

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () =>{
 
                       <div className="eight wide column">
                         <h4 className="ui header">Covid Data Sources</h4>
-                        <div style={{marginTop: '20px'}}>
+                        <div >
                         <div className="ui link list" style={{margin: 'auto'}}>
                             <p className="sources">County Data : <a href="https://github.com/nytimes/covid-19-data" className="item">New York Times Covid-19 Data</a> </p>
                             <br/>
@@ -29,26 +29,31 @@ const Footer = () =>{
                         <h4 className="ui header">Sources</h4>
                         <div className="ui link list">
 
-                          <p className="sources"style={{marginBottom: '-15px'}}> <a href="#" className="item">County Income Data </a> </p>
-                            <br/>
-                          <p className="sources"style={{marginBottom: '-15px'}}> <a href="#" className="item"> State Income Data </a> </p>
-                            <br/>
-                          <p className="sources"style={{marginBottom: '-15px'}}>  <a href="#" className="item">County Population Data </a> </p>
-                            <br/>
-                          <p className="sources" style={{marginBottom: '-35px'}}> <a href="#" className="item">State Population Data</a> </p>
-                            <br/>
+                        <div className="sourceLayout" >
+                            <p className="sources"style={{marginBottom: '-15px', marginRight: '10px'}}> <a href="#" target="_blank" className="item">County Income Data </a> </p>
+                            <p className="sources"style={{marginBottom: '-15px'}}> <a href="#" target="_blank" className="item"> State Income Data </a> </p>
+                              <br/>
+                        </div>
 
+                      <div className="sourceLayout">
+                          <p className="sources"style={{marginBottom: '-15px',  marginRight: '10px'}}>  <a href="#" target="_blank" className="item">County Population Data </a> </p>
+                            <br/>
+                          <p className="sources" style={{marginBottom: '-35px'}}> <a href="#" target="_blank" className="item">State Population Data</a> </p>
+                            <br/>
                         </div>
                       </div>
 
+
+                      </div>
+
                     </div>
-                      <div className="ui inverted section divider"></div>
+                      <div className="ui inverted section divider" style={{marginTop: '10px', marginBottom: '15px'}}></div>
 
                        <div className="ui horizontal small divided link list">
                        <h4 className="ui header" style={{fontSize: '20px', marginTop: '-20px'}}>Designed and Implemented by Daniel Niedzwiedzki </h4>
-                        <a className="item" href="https://github.com/ndnied91" target="_blank"> <i class="github large icon"></i>  </a>
-                        <a className="item" href="https://www.linkedin.com/in/daniel-niedzwiedzki/" target="_blank"> <i class="linkedin large icon "></i> </a>
-                        <a href= "mailto: danielniedzwiedzki.1@gmail.com" className="item" > <i class="envelope large icon "></i> </a>
+                        <a className="item" href="https://github.com/ndnied91" target="_blank"> <i className="github large icon"></i>  </a>
+                        <a className="item" href="https://www.linkedin.com/in/daniel-niedzwiedzki/" target="_blank"> <i className="linkedin large icon "></i> </a>
+                        <a href= "mailto: danielniedzwiedzki.1@gmail.com" className="item" > <i className="envelope large icon "></i> </a>
                        </div>
 
 
