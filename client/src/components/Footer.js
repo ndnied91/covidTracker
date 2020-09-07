e50b264b562729f3c5814b66cb62923689187289
danny <danny@Dannys-MacBook-Pro.local>
2020-09-07 14:11:23 -0400

    added links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -30,15 +30,15 @@ const Footer = () =>{
                         <div className="ui link list">
 
                         <div className="sourceLayout" >
-                            <p className="sources"style={{marginBottom: '-15px', marginRight: '10px'}}> <a href="#" target="_blank" className="item">County Income Data </a> </p>
-                            <p className="sources"style={{marginBottom: '-15px'}}> <a href="#" target="_blank" className="item"> State Income Data </a> </p>
+                            <p className="sources"style={{marginBottom: '-15px', marginRight: '10px'}}> <a href="https://data.ers.usda.gov/reports.aspx?ID=17828" target="_blank" className="item">County Median Income Data </a> </p>
+                            <p className="sources"style={{marginBottom: '-15px'}}> <a href="https://dqydj.com/average-income-by-state-median-top-percentiles/" target="_blank" className="item"> State Median Income Data </a> </p>
                               <br/>
                         </div>
 
                       <div className="sourceLayout">
-                          <p className="sources"style={{marginBottom: '-15px',  marginRight: '10px'}}>  <a href="#" target="_blank" className="item">County Population Data </a> </p>
+                          <p className="sources"style={{marginBottom: '-15px',  marginRight: '10px'}}>  <a href="https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html" target="_blank" className="item">County Population Data </a> </p>
                             <br/>
-                          <p className="sources" style={{marginBottom: '-35px'}}> <a href="#" target="_blank" className="item">State Population Data</a> </p>
+                          <p className="sources" style={{marginBottom: '-35px'}}> <a href="https://www.census.gov/data/tables/time-series/demo/popest/2010s-state-total.html" target="_blank" className="item">State Population Data</a> </p>
                             <br/>
                         </div>
                       </div>
