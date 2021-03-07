64f3080f9a9387fdee6f254d2f9fc41fcaa0ff8b
danny <danny@Dannys-MacBook-Pro.local>
2021-03-06 19:00:22 -0500

    minor updates

--- config/dev.js
+++ config/dev.js
@@ -1,16 +1,4 @@
 module.exports = {
-    mongoURI:"mongodb+srv://danny:Maya2020@covid-data.htp8j.mongodb.net/covid-data?retryWrites=true&w=majority"
+    mongoURI:""
 }
-
-
-
-  // mongoURI:'mongodb://danny:Maya2019@ds123664.mlab.com:23664/covid_data'
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://danny:<password>@covid-data.htp8j.mongodb.net/<dbname>?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+//database credentials go here
